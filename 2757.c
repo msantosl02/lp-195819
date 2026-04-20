85c221dae5762ca814c057609071105008f108e3
msantosl02 <mateus.lucas02@unifesp.br>
2026-04-20 06:04:58 -0300

    Update 2757.c

--- 2757.c
+++ 2757.c
@@ -4,7 +4,7 @@ Nome        : Mateus Santos Lucas
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/2757
 Data        : 14/04/2026
-Objetivo    : Ler e imprimir valores com diferentes condições
+Objetivo    : Ler e imprimir valores com diferentes condições.
 Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
 -------------------------------------------------------------------------- */
 
