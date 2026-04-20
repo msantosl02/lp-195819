a37db1a55e111102f043f672b2fef674860933a4
msantosl02 <mateus.lucas02@unifesp.br>
2026-04-20 05:54:38 -0300

    Update 1080b.c

--- 1080b.c
+++ 1080b.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080b
+Data        : 17/04/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 int main() {
 	int X[100] , i , maior , ps;
