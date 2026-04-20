e640d110267cf0e3f01b327dc936a4045cf67f70
msantosl02 <mateus.lucas02@unifesp.br>
2026-04-20 06:04:14 -0300

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1013
+Data        : 12/04/2026
+Objetivo    : Fazer um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”.
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+-------------------------------------------------------------------------- */
+
 #include "stdio.h"
 
 int main()
