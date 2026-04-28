7c8bc7bad2e0b6a2a43a6343bfdaec8cc0b835a0
msantosl02 <mateus.lucas02@unifesp.br>
2026-04-28 16:29:46 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -5,10 +5,10 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 12/04/2026
 Objetivo    : Imprimir o texto "Hello world!" na tela
-Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+Aprendizado : Uso do comando printf
 -------------------------------------------------------------------------- */
 
-#include "stdio.h"
+#include <stdio.h>
 
 int main()
 {
