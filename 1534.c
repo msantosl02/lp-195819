123631f1f6b10328e1ca1e440b00fd16939fe359
msantosl02 <mateus.lucas02@unifesp.br>
2026-05-14 15:01:02 -0300

    Update 1534.c

--- 1534.c
+++ 1534.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1534
+Data        : 14/05/2026
+Objetivo    : Ler um valor inteiro N que é o tamanho da matriz que deve ser impressa.
+Aprendizado : Uso do comando while.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 int main () {
 	int N, i, j;
