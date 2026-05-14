ff2fdc5ff8522e502de1029dca708dbb3531d5d1
msantosl02 <mateus.lucas02@unifesp.br>
2026-05-14 14:44:39 -0300

    Update 1015.c

--- 1015.c
+++ 1015.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1015
+Data        : 14/05/2026
+Objetivo    : Ler 4 valores x1, y1, x2, y2 e calcular a distância entre eles, mostrando 4 casas decimais.
+Aprendizado : Como calcular a distância entre dois pontos.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <math.h>
 int main() {
