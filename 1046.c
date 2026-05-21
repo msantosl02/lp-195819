dee676fb1ce5d28949b29cfae1dacecbe1e2fc55
msantosl02 <mateus.lucas02@unifesp.br>
2026-05-21 15:29:30 -0300

    Update 1046.c

--- 1046.c
+++ 1046.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1046
+Data        : 21/05/2026
+Objetivo    : Receber valores e com base neles calcular a duração de um jogo.
+Aprendizado : 
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
