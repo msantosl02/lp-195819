68629004bb77d7b7dde9b87928d9dd1a3ccdf942
msantosl02 <mateus.lucas02@unifesp.br>
2026-05-25 14:07:47 -0300

    Create 1179.c

---
+++ 1179.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int par[5], impar[5];
+    int qtd_par = 0, qtd_impar = 0;
+    int num;
+
+    for (int i = 0; i < 15; i++) {
+        scanf("%d", &num);
+
+        if (num % 2 == 0) {
+            par[qtd_par] = num;
+            qtd_par++;
+            if (qtd_par == 5) {
+                for (int j = 0; j < 5; j++) {
+                    printf("par[%d] = %d\n", j, par[j]);
+                }
+                qtd_par = 0;
+            }
+        } else {
+            impar[qtd_impar] = num;
+            qtd_impar++;
+            if (qtd_impar == 5) {
+                for (int j = 0; j < 5; j++) {
+                    printf("impar[%d] = %d\n", j, impar[j]);
+                }
+                qtd_impar = 0;
+            }
+        }
+    }
+
+    for (int j = 0; j < qtd_impar; j++) {
+        printf("impar[%d] = %d\n", j, impar[j]);
+    }
+
+    for (int j = 0; j < qtd_par; j++) {
+        printf("par[%d] = %d\n", j, par[j]);
+    }
+
+    return 0;
+}
