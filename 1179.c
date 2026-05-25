2ddf50dc7b6309b25ed43a82d515d2d37a22f50a
msantosl02 <mateus.lucas02@unifesp.br>
2026-05-25 14:09:29 -0300

    Update 1179.c

--- 1179.c
+++ 1179.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1179
+Data        : 25/05/2026
+Objetivo    : 
+Aprendizado : 
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
