7494638f9f9ab7ab88f5aa25f563ba21835a25b4
msantosl02 <mateus.lucas02@unifesp.br>
2026-06-03 14:38:19 -0300

    Atualizar o 1441.c

--- 1441.c
+++ 1441.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas 
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1441
+Data        : 03/05/2026
+Objetivo    : 
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
