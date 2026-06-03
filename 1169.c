064d39d1560016936ed622de96d3e732deb8bc5a
msantosl02 <mateus.lucas02@unifesp.br>
2026-06-03 14:30:34 -0300

    Criar 1169.c

---
+++ 1169.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    int x;
+    unsigned long long graos;
+    unsigned long long kg;
+
+    if (scanf("%d", &n) != EOF) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &x);
+
+            if (x == 64) {
+                printf("1537228672809129 kg\n");
+            } else {
+                graos = (1ULL << x) - 1;
+                
+                kg = graos / 12 / 1000;
+
+                printf("%llu kg\n", kg);
+            }
+        }
+    }
+
+    return 0;
+}
