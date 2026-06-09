6a2787073c903e06b17fa6fca5c01aae02edfd9b
msantosl02 <mateus.lucas02@unifesp.br>
2026-06-09 19:12:02 -0300

    Atualizar o 1030.c

--- 1030.cpp
+++ 1030.cpp
@@ -1,37 +1,32 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Mateus Santos Lucas 
-Linguagem   : C++
+Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1030
 Data        : 09/06/2026
 Objetivo    : 
 Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
 Pergunta de segunda ordem: <<<Fazer a pergunta>>>
 -------------------------------------------------------------------------- */
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int josepheus(int n, int k) {
+int josephus(int n, int k) {
     int sobrevivente = 0;
-    
-    for (int i=2; i<=n; i++) {
+    for (int i = 2; i <= n; ++i) {
         sobrevivente = (sobrevivente + k) % i;
     }
     return sobrevivente + 1;
 }
-    
-    int main() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        int nc;
-        if (cin>>nc) {
-            for(int case_num = 1; case_num <=nc; ++case_num) {
-                int n, k;
-                cin >> n >> k;
-                cout <<"Case " << case_num << ": " << josepheus(n,k) <<"\n";
+
+int main() {
+    int nc;
+    if (scanf("%d", &nc) == 1) {
+        for (int case_num = 1; case_num <= nc; ++case_num) {
+            int n, k;
+            if (scanf("%d %d", &n, &k) == 2) {
+                printf("Case %d: %d\n", case_num, josephus(n, k));
             }
-                
         }
+    }
     return 0;
 }
