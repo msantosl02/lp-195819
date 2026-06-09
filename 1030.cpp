12614e95a695e1c96bc31f9e598cd31c14040814
msantosl02 <mateus.lucas02@unifesp.br>
2026-06-09 16:31:09 -0300

    Update 1030.cpp

--- 1030.cpp
+++ 1030.cpp
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mateus Santos Lucas 
+Linguagem   : C++
+Problema    : https://judge.beecrowd.com/pt/problems/view/1030
+Data        : 09/06/2026
+Objetivo    : 
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
 #include <iostream>
 using namespace std;
 
